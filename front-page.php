26a39b5e092a525bbefe64455ada882b3ae9b7a8
Guy Ytzhak <gytzhak.seo@gmail.com>
2018-06-28 02:44:12 +0300

    Change Html Markup

--- front-page.php
+++ front-page.php
@@ -2,20 +2,14 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php the_post_thumbnail('full'); ?>
 
-    <div id="page_content" class="home win_height" role="main">
-        <div class="container-fluid">
+    <div id="page_content" class="home" role="main">
+        <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-md-8 middle_content win_height">
+                <div class="col-xs-12">
                     <div class="inner_content">
                         <h1><?php the_title(); ?></h1>
                         <?php the_content(); ?>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-md-4 home_sidebar middle_content win_height">
-                    <div class="inner_content">
-                        Register Side
                     </div>
                 </div>
             </div>
