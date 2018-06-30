7016d2d1186e305305fd27aaa8880f599ea42c64
Guy Ytzhak <gytzhak.seo@gmail.com>
2018-06-30 03:15:53 +0300

    Update woo_func.php

--- inc/woo_func.php
+++ inc/woo_func.php
@@ -1,12 +1,44 @@
 <?php 
 
 // Remove each style one by one
-add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
-function jk_dequeue_styles( $enqueue_styles ) {
+add_filter( 'woocommerce_enqueue_styles', 'cs_dequeue_styles' );
+function cs_dequeue_styles( $enqueue_styles ) {
 	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
 	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
 	unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
 	return $enqueue_styles;
 }
 
-
+add_action( 'wp_enqueue_scripts', 'cs_manage_woocommerce_styles', 99 );
+  
+function cs_manage_woocommerce_styles() {
+    //remove generator meta tag
+    remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+  
+    //first check that woo exists to prevent fatal errors
+    if ( function_exists( 'is_woocommerce' ) ) {
+        //dequeue scripts and styles
+        if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+            wp_dequeue_style( 'woocommerce_frontend_styles' );
+            wp_dequeue_style( 'woocommerce_fancybox_styles' );
+            wp_dequeue_style( 'woocommerce_chosen_styles' );
+            wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+            wp_dequeue_script( 'wc_price_slider' );
+            wp_dequeue_script( 'wc-single-product' );
+            wp_dequeue_script( 'wc-add-to-cart' );
+            wp_dequeue_script( 'wc-cart-fragments' );
+            wp_dequeue_script( 'wc-checkout' );
+            wp_dequeue_script( 'wc-add-to-cart-variation' );
+            wp_dequeue_script( 'wc-single-product' );
+            wp_dequeue_script( 'wc-cart' );
+            wp_dequeue_script( 'wc-chosen' );
+            wp_dequeue_script( 'woocommerce' );
+            wp_dequeue_script( 'prettyPhoto' );
+            wp_dequeue_script( 'prettyPhoto-init' );
+            wp_dequeue_script( 'jquery-blockui' );
+            wp_dequeue_script( 'jquery-placeholder' );
+            wp_dequeue_script( 'fancybox' );
+            wp_dequeue_script( 'jqueryui' );
+        }
+    }
+ }
